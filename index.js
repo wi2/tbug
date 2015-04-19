61fcbcfc014e806b57d218f715e7a0e461ab6c54
wi2 <contact@wi2prod.fr>
2015-04-19 12:43:28 +0200

    add filename and linenumber

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 var ms = require('ms')
   , start = +(new Date())
   , list = []
-  , count = 0;
+  , count = 0
+  , orig = Error.prepareStackTrace
+  , err = new Error;
+
+var errorTrace = function(){
+  Error.prepareStackTrace = function(_, stack){ return stack; };
+  Error.captureStackTrace(err, arguments.callee);
+  var stack = err.stack;
+  Error.prepareStackTrace = orig;
+  if (stack[0].getFileName() !== stack[1].getFileName())
+    return stack[1].getFileName() + " " + stack[1].getLineNumber() + " " + (stack[1].getFunctionName() ? " - "+stack[1].getFunctionName()+"()" : "");
+  else
+    return null;
+}
 
 var debug = function(what, opt) {
 
@@ -33,14 +46,17 @@ var debug = function(what, opt) {
   }
 
   var worker = function() {
-    var vals = Array.prototype.slice.call(arguments, 0);
-    var dif = +(new Date()) - start;
+    var vals = Array.prototype.slice.call(arguments, 0)
+      , dif = +(new Date()) - start
+      , tmpTrace = errorTrace();
+
+    if (tmpTrace !== undefined)
+      list.push("\n\n-- " + tmpTrace + "\n-- " + context.toString() + " " + ms(dif));
+      for(var i=0,len=vals.length; i<len; i++)
+        list.push(vals[i]);
     start = +(new Date());
-    list.push("\n\n----\n" + context.toString() + " " + ms(dif));
-    // list.concat(vals);
-    for(var i=0,len=vals.length; i<len; i++)
-      list.push(vals[i]);
   }
+
   //init
   if (check()) {
     worker( ms(start) );
